83767f85c57ea08971d37ab88e0fcbd0d82f158d
Joaovls <joaovictor02021512@gmail.com>
2026-06-18 12:19:59 -0300

    chore: configura api em producao

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://supportdesk-api-14g8.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
